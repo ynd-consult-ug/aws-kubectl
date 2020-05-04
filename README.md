b7b0c16f768784cb7edd37e6b8cf7cc516708e16
Tomasz Lagodzinski <tomasz.lagodzinski@ynd.co>
2020-05-04 14:47:55 +0200

    Fixes and improvements.

--- README.md
+++ README.md
@@ -2,16 +2,19 @@ The Docker Image contains the aws-cli and kubectl. It is used to update the AWS 
 
 # Setup
 
-You need to set your credentials in the aws-secrets.yml. Also you need to set your AWS_ACCOUNT, AWS_REGION and NAMESPACES in ecr-cron.yml.
-Afterwords run:
+Run and answer all prompts:
 
-	aws.sh
+  ./aws.sh
 
-Afterwords you should be able to see the cron job with:
+or provide the values like this:
 
-	kubectl get cronjobs -n infrastructure
+  AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=KEY AWS_REGION=eu-central-1 AWS_ACCOUNT=ID NAMESPACES='prod test' ./aws.sh
+
+Afterwards you should be able to see the cron job with:
+
+  kubectl get cronjobs
 
 # Thanks
 
-This is based on the work of @xynova.
+This is based on the work of @xynova and Mike Petersen <mike@odania-it.de>.
 
--- aws.sh
+++ aws.sh
@@ -1,15 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash -x
 DIR=$(dirname $0)
 cd $DIR
 
-echo "Recreating aws-secrets"
-kubectl delete -n infrastructure -f templates/aws-secrets.yml || true
-kubectl create -n infrastructure -f templates/aws-secrets.yml
+if [ -z ${AWS_ACCOUNT+x} ];then
+  read -p 'AWS Account ID: ' AWS_ACCOUNT
+fi
+if [ -z ${AWS_REGION+x} ];then
+  read -p 'AWS Region: ' AWS_REGION
+fi
+if [ -z ${NAMESPACES+x} ];then
+  read -p 'Namespaces (space separated): ' NAMESPACES
+fi
+if [ -z ${AWS_ACCESS_KEY_ID+x} ];then
+  read -p 'Please provide AWS_ACCESS_KEY_ID: ' AWS_ACCESS_KEY_ID
+fi
+if [ -z ${AWS_SECRET_ACCESS_KEY+x} ];then
+  read -sp 'Please provide AWS_SECRET_ACCESS_KEY: ' AWS_SECRET_ACCESS_KEY
+fi
 
-echo "Recreating aws-role"
-kubectl delete -n infrastructure -f templates/aws-role.yml || true
-kubectl create -n infrastructure -f templates/aws-role.yml
+echo "Creating aws-secrets"
+kubectl create secret generic aws-secrets \
+  --from-literal="aws-access-key-id=$AWS_ACCESS_KEY_ID" \
+  --from-literal="aws-secret-access-key=$AWS_SECRET_ACCESS_KEY" \
+  --from-literal="aws-region=$AWS_REGION" \
+  --from-literal="aws-account=$AWS_ACCOUNT" \
+  --from-literal="namespaces=\'$NAMESPACES\'" \
+  --dry-run=client \
+  -o yaml| kubectl apply -f-
 
-echo "Recreating infrastructure aws-ecr-cron"
-kubectl delete -n infrastructure -f templates/ecr-cron.yml || true
-kubectl create -n infrastructure -f templates/ecr-cron.yml
+echo "Upserting aws-role"
+kubectl apply -f templates/aws-role.yml
+
+echo "Upserting $NAMESPACE aws-ecr-cron"
+kubectl apply -f templates/ecr-cron.yml
